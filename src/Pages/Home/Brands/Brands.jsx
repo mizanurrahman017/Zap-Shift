d3fb46256f676bef86e6175de2ff2d757fc7702a
unknown <mizanasif1@gmail.com>
2026-07-12 21:36:49 +0600

    more code added

---
+++ src/Pages/Home/Brands/Brands.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+const Brands = () => {
+    return (
+       <Swiper>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+       </Swiper>
+    );
+};
+
+export default Brands;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Works from '../Work/Works';
 import Service from '../Service/Service';
+import Brands from '../Brands/Brands';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
           <Banner></Banner>
           <Works></Works>
           <Service></Service>
+          <Brands></Brands>
         </div>
     );
 };
